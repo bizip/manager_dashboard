6b6d19fd5a4fb46d0847bfb1e269e1a8e5114c1b
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2022-10-30 12:06:40 +0200

    Add all dashboard figures:
    -Add a bar chart with labels
    -Add a donut/pie chart with percentages of sections
    -Add a progress line bar
    
    [Done]

---
+++ src/components/shared/Donutchart.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { CircularProgressbarWithChildren } from 'react-circular-progressbar';
+
+const Donutchart = () => (
+  <div>
+    <CircularProgressbarWithChildren value={66}>
+      <div style={{ fontSize: 12, marginTop: -5 }} className="center">
+        <strong>
+          Percent
+        </strong>
+        <strong>
+          78
+        </strong>
+      </div>
+    </CircularProgressbarWithChildren>
+  </div>
+);
+
+export default Donutchart;
